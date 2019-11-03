8cb408cedfac090ae6420ed88801618a60991fc8
Suresh Kumar <48355082+SureshKumar-CSE@users.noreply.github.com>
2019-11-03 17:58:49 +0530

    Update batteryAlert.sh

--- batteryAlert.sh
+++ batteryAlert.sh
@@ -8,18 +8,15 @@ ac_power=$(acpi -b|grep -c "Discharging")
 #echo $ac_power
 
 #When the battery is charging and it gets charged up to 100%
-
 if [[ $ac_power -eq 0 && $battery_level -eq 100 ]]
 then
 echo "Battery is full." "Level: $battery_level% " > batteryStatus.txt
 echo "Charge full, please remove the charger." >> batteryStatus.txt
-
 fi
-#When the battery is not charging and it goes below 30%
 
+#When the battery is not charging and it goes below 30%
 if [[ $ac_power -eq 1 && $battery_level -lt 30 ]]
 then
 echo "Battery is low." "Level: $battery_level% " > batteryStatus.txt
 echo "Please connect the charger." >> batteryStatus.txt
-
 fi
